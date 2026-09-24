cfc5d27931bf32cf9bf22861a864804e288b9a1e
yousifayadh <yousifayadh@gmail.com>
2026-09-23 22:25:18 -0700

    Updated the ArrayList.h file

---
+++ ArrayList.h
@@ -0,0 +1,74 @@
+//
+// Created by nawfa on 9/15/2026.
+//
+
+# pragma once
+
+# include <iostream>
+#include "List.h"
+
+template <typename T>
+
+class ArrayList : public List<T>
+{
+    void addFront(T* value) override
+    {
+        if (size_ >= CAPACITY)
+        {
+            std::cout <<"ArrayList is full." << std::endl;
+            return;
+        }
+        for (int i = size_; i > 0; --i)
+        {
+            data_[i] = data_[i - 1];
+        }
+        data_[0] = value;
+        ++size_;
+    }
+
+    void deleteFront() override
+    {
+        if (size == 0)
+        {
+            std::cout <<"ArrayList is empty." << std::endl;
+            return;
+        }
+        delete data_[0];
+        for (int i = 0; i < size_ - 1; ++i) {
+            data_[i] = data_[i + 1];
+        }
+        --size_;
+    }
+
+    bool search(T* value) const override
+    {
+        for (int i = 0; i < size_; ++i)
+        {
+            if (*data_[i] == *value) return true;
+        }
+        return false;
+    }
+
+    void print() const override
+    {
+        for (int i = 0; i < size_; ++i)
+        {
+            std::cout << *data_[i] << ",";
+        }
+        std::cout << std::endl;
+    }
+    ~ArrayList() override
+    {
+        for (int i = 0; i < size_; ++i)
+        {
+            delete data_[i];
+        }
+    }
+private:
+    static const int CAPACITY = 20;
+    T* data_[CAPACITY];
+    int size_;
+};
+
+
+
